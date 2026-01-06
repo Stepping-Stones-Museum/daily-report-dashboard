ca091051b192ffefa5ddaf3b3333f396118a74cf
haydnkerr <haydnkerr365@gmail.com>
2026-01-06 14:47:09 -0500

    Enhanced YTD Section
    This includes YTD for shops, birthdays, membership etc

--- script.js
+++ script.js
@@ -41,6 +41,23 @@ let currentBirthdayRevenue;
 let currentGroupRevenue;
 let totalRevenue;
 
+const monthPercents = {
+    January: 1.20,
+    February: 1.10,
+    March: 1.20,
+    April: 1.20,
+    May: 1.20,
+    June: 1.20,
+    July: 1.20,
+    August: 1.20,
+    September: 1.20,
+    October: 1.20,
+    November: 1.20,
+    December: 1.20
+};
+
+
+
 
 // Fetch data
 function fetchUrlData(urlData) {
@@ -55,9 +72,51 @@ function fetchUrlData(urlData) {
 
 
 
-            const ytdRevenue = calculateYTDRevenue(data, currentDate);
+            // const ytdRevenue = calculateYTDRevenue(data, currentDate);
+            // populateRevenueMeter(ytdRevenue, yearlyGoal);
 
-            populateRevenueMeter(ytdRevenue, yearlyGoal);
+            const ytdTotalRevenue = calculateYTDByKey(data, currentDate, 'total_revenue');
+            const ytdMembersRevenue = calculateYTDByKey(data, currentDate, 'membership_revenue');
+            const ytdGroupRevenue = calculateYTDByKey(data, currentDate, 'group_school_revenue');
+            const ytdBirthdayRevenue = calculateYTDByKey(data, currentDate, 'birthday_revenue');
+            const ytdSchoolsRevenue = calculateYTDByKey(data, currentDate, 'group_school_revenue');
+
+
+            populateRevenueMeter({
+                ytdRevenue: ytdTotalRevenue,
+                yearlyGoal: 100_000,
+                fillSelector: '.total-revenue-fill',
+                labelSelector: '.total-revenue-meter-label'
+            });
+
+            populateRevenueMeter({
+                ytdRevenue: ytdMembersRevenue,
+                yearlyGoal: 20_000,
+                fillSelector: '.membership-revenue-fill',
+                labelSelector: '.membership-revenue-meter-label'
+            });
+
+            populateRevenueMeter({
+                ytdRevenue: ytdGroupRevenue,
+                yearlyGoal: 20_000,
+                fillSelector: '.shop-revenue-fill',
+                labelSelector: '.shop-revenue-meter-label'
+            });
+
+            populateRevenueMeter({
+                ytdRevenue: ytdBirthdayRevenue,
+                yearlyGoal: 20_000,
+                fillSelector: '.birthday-revenue-fill',
+                labelSelector: '.birthday-revenue-meter-label'
+            });
+
+            populateRevenueMeter({
+                ytdRevenue: ytdSchoolsRevenue,
+                yearlyGoal: 20_000,
+                fillSelector: '.group-revenue-fill',
+                labelSelector: '.group-revenue-meter-label'
+            });
+
 
 
             document.querySelector(".daily-date-heading").textContent = formatDate(currentDate);
@@ -85,26 +144,30 @@ function fetchUrlData(urlData) {
 
                 const dayName = data[dateStr]?.day?.slice(0, 3);
 
+                const adjustedAttendance = forecastPercentageIncrease(dateStr, attendance)
+
 
                 return {
                     date: dateStr,
-                    attendance,
+                    adjustedAttendance,
                     dayName
                 };
             });
 
-            // Map revenue breakdown to an object
-            const dayData = data[currentDate];
+            console.log(forecastData)
 
-            const revenueBreakdowndata = {
-                books: dayData?.books_revenue ?? 0,
-                creativePlay: dayData?.creative_play_revenue ?? 0,
-                novelty: dayData?.novelty_revenue ?? 0,
-                plush: dayData?.plush_revenue ?? 0,
-                puzzles: dayData?.puzzles_games_revenue ?? 0,
-                steppingStones: dayData?.stepping_stones_revenue ?? 0,
-                toys: dayData?.toys_revenue ?? 0
-            };
+            // Map revenue breakdown to an object
+            // const dayData = data[currentDate];
+
+            // const revenueBreakdowndata = {
+            //     books: dayData?.books_revenue ?? 0,
+            //     creativePlay: dayData?.creative_play_revenue ?? 0,
+            //     novelty: dayData?.novelty_revenue ?? 0,
+            //     plush: dayData?.plush_revenue ?? 0,
+            //     puzzles: dayData?.puzzles_games_revenue ?? 0,
+            //     steppingStones: dayData?.stepping_stones_revenue ?? 0,
+            //     toys: dayData?.toys_revenue ?? 0
+            // };
 
 
             currentGeneralRevenue = parseInt(data[currentDate]?.general_admission_revenue || 0);
@@ -114,13 +177,13 @@ function fetchUrlData(urlData) {
             currentGroupRevenue = parseInt(data[currentDate]?.group_school_revenue || 0);
             totalRevenue = parseInt(data[currentDate]?.total_revenue || 0);
 
+
+
             populateBarGraph(twoYearAttendanceData, oneYearAttendanceData, currentAttendanceData);
             populateRevenuePie(currentGeneralRevenue, curentMemberRevenue, currentShopRevenue, currentBirthdayRevenue, currentGroupRevenue, totalRevenue);
             populateForecastBarGraph(forecastData)
-            populateRevenueBreakdownBarGraph(revenueBreakdowndata)
 
-            console.log("Rendered Info", data);
-            console.log("Current Attendance", currentAttendanceData);
+            // populateRevenueBreakdownBarGraph(revenueBreakdowndata)
         })
         .catch(err => console.error("Failed to fetch schedule data:", err));
 }
@@ -169,10 +232,31 @@ function populateBarGraph(one, two, three) {
     });
 }
 
-// Forecast Bar graph
+
+
+// Forecast Bar graph Section
+// Adjusting percentage increase
+function forecastPercentageIncrease(dateStr, forecastIncreaseData) {
+    const [y, m, d] = dateStr.split('-').map(Number);
+    const date = new Date(y, m - 1, d);
+
+    let forecastIncreaseDate = date.toLocaleString('en-US', { month: 'long' });
+    // console.log("Date", date)
+    // console.log("forecast Date", forecastIncreaseDate)
+
+    adjustedForecastData = Math.floor(
+        forecastIncreaseData * ((monthPercents[forecastIncreaseDate] ?? 1))
+    );
+
+
+
+    return adjustedForecastData;
+}
+
+// Poplating Bar Graphs
 function populateForecastBarGraph(forecastData) {
     forecastData.forEach((day, i) => {
-        document.querySelectorAll(".forecast-bar-label")[i].textContent = day.attendance;
+        document.querySelectorAll(".forecast-bar-label")[i].textContent = day.adjustedAttendance;
         document.querySelectorAll(".forecast-label")[i].textContent = day.dayName;
     });
 
@@ -184,8 +268,6 @@ function populateForecastBarGraph(forecastData) {
     let fivePercentage = attendancePercentage(fiveDayDate);
     let sixPercentage = attendancePercentage(sixDayDate);
     let sevenPercentage = attendancePercentage(sevenDayDate);
-
-
 
 
     const barPercentages = [onePercentage, twoPercentage, threePercentage, fourPercentage, fivePercentage, sixPercentage, sevenPercentage];
@@ -221,8 +303,6 @@ function populateRevenuePie(one, two, three, four, five, totalRevenue) {
 
     let gradient = 'conic-gradient(';
     let start = 0;
-
-
 
     document.querySelector(".pie-revenue-total").textContent = totalRevenue;
 
@@ -280,17 +360,17 @@ function renderForecast(date) {
 }
 
 function renderDates() {
-    console.log('Current:', currentDate);
-    console.log('1 Year Ago:', oneYearPreviousDate);
-    console.log('2 Years Ago:', twoYearPreviousDate);
+    // console.log('Current:', currentDate);
+    // console.log('1 Year Ago:', oneYearPreviousDate);
+    // console.log('2 Years Ago:', twoYearPreviousDate);
 
-    console.log('1 day:', oneDayDate);
-    console.log('2 day:', twoDayDate);
-    console.log('3 day:', threeDayDate);
-    console.log('4 day:', fourDayDate);
-    console.log('5 day:', fiveDayDate);
-    console.log('6 day:', sixDayDate);
-    console.log('7 day:', sevenDayDate);
+    // console.log('1 day:', oneDayDate);
+    // console.log('2 day:', twoDayDate);
+    // console.log('3 day:', threeDayDate);
+    // console.log('4 day:', fourDayDate);
+    // console.log('5 day:', fiveDayDate);
+    // console.log('6 day:', sixDayDate);
+    // console.log('7 day:', sevenDayDate);
 
     fetchUrlData(firebaseFetchUrl);
 }
@@ -303,37 +383,81 @@ document.addEventListener('DOMContentLoaded', () => {
 
 });
 
+function parseLocalDate(dateStr) {
+    const [year, month, day] = dateStr.split("-").map(Number);
+    return new Date(year, month - 1, day); // LOCAL midnight
+}
 
 
-function calculateYTDRevenue(data, currentDateStr) {
-    const currentDateObj = new Date(currentDateStr);
-    currentYear = currentDateObj.getFullYear();
-    console.log(currentYear)
 
-    let ytdRevenue = 0;
+
+// function calculateYTDRevenue(data, currentDateStr) {
+//     const currentDateObj = parseLocalDate(currentDateStr);
+//     const currentYear = currentDateObj.getFullYear();
+
+//     let ytdRevenue = 0;
+
+//     Object.entries(data).forEach(([dateStr, dayData]) => {
+//         const dateObj = parseLocalDate(dateStr);
+
+//         if (
+//             dateObj.getFullYear() === currentYear &&
+//             dateObj <= currentDateObj
+//         ) {
+//             ytdRevenue += Number(dayData?.total_revenue || 0);
+//         }
+//     });
+
+//     return ytdRevenue;
+// }
+
+
+
+// function populateRevenueMeter(ytdRevenue, yearlyGoal) {
+//     const percentage = Math.min((ytdRevenue / yearlyGoal) * 100, 100);
+//     console.log("Second Rev", ytdRevenue)
+//     console.log("Rev Goal", yearlyGoal)
+//     document.querySelector('.total-revenue-fill').style.height =
+//         `${percentage}%`;
+
+//     document.querySelector('.total-revenue-meter-label').textContent =
+//         `$${ytdRevenue.toLocaleString()} (${percentage.toFixed(1)}%)`;
+// }
+
+function calculateYTDByKey(data, currentDateStr, revenueKey) {
+    const currentDateObj = parseLocalDate(currentDateStr);
+    const currentYear = currentDateObj.getFullYear();
+
+    let ytdTotal = 0;
 
     Object.entries(data).forEach(([dateStr, dayData]) => {
-        const dateObj = new Date(dateStr);
-        if (dateObj.getFullYear() === currentYear && dateObj <= currentDateObj) {
-            ytdRevenue += parseInt(dayData?.total_revenue || 0);
+        const dateObj = parseLocalDate(dateStr);
+
+        if (
+            dateObj.getFullYear() === currentYear &&
+            dateObj <= currentDateObj
+        ) {
+            ytdTotal += Number(dayData?.[revenueKey] || 0);
         }
     });
 
-    return ytdRevenue;
+    return ytdTotal;
 }
 
-
-
-
-function populateRevenueMeter(ytdRevenue, yearlyGoal) {
+function populateRevenueMeter({
+    ytdRevenue,
+    yearlyGoal,
+    fillSelector,
+    labelSelector
+}) {
     const percentage = Math.min((ytdRevenue / yearlyGoal) * 100, 100);
 
-    document.querySelector('.revenue-meter-fill').style.height =
-        `${percentage}%`;
-
-    document.querySelector('.revenue-meter-label').textContent =
-        `$${ytdRevenue.toLocaleString()} of $${yearlyGoal.toLocaleString()} (${percentage.toFixed(1)}%)`;
+    document.querySelector(fillSelector).style.height = `${percentage}%`;
+    document.querySelector(labelSelector).textContent =
+        `$${ytdRevenue.toLocaleString()} (${percentage.toFixed(1)}%)`;
 }
+
+
 
 
 
